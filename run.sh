fbba4b6032c640b0dd339015e185ec337d7fbde2
termie <github@anarkystic.com>
2016-09-19 20:18:34 +0200

    initial

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+source /init.sh
+
+BASE_URL=https://app.wercker.com
+OWNER=wercker
+NAME=bash-template
+FULL_URL="${BASE_URL}/api/v2/steps/${OWNER}/${NAME}/*"
+TARBALL_URL=$(curl "${FULL_URL}" | jq -r '.tarballUrl')
+
+# cd to the workspace
+cd "$1" || exit
+
+WORKING=working
+rm -rf ${WORKING}
+mkdir -p ${WORKING}/output ${WORKING}/cache ${WORKING}/report
+WERCKER_ROOT=$(pwd)
+WERCKER_SOURCE_DIR="${WERCKER_ROOT}"
+WERCKER_OUTPUT_DIR="${WERCKER_ROOT}/${WORKING}/output"
+WERCKER_REPORT_DIR="${WERCKER_ROOT}/${WORKING}/report"
+WERCKER_CACHE_DIR="${WERCKER_ROOT}/${WORKING}/cache"
+
+WERCKER="false"
+WERCKER_STEP_ROOT="${WERCKER_ROOT}/${WORKING}"
+WERCKER_STEP_NAME="${NAME}"
+WERCKER_STEP_OWNER="${OWNER}"
+
+cd "${WORKING}" || exit
+curl -o step.tar "${TARBALL_URL}"
+tar -xvf step.tar
+cd ..
+source "${WERCKER_ROOT}/${WORKING}/run.sh"
